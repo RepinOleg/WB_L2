6b8c89dd7731a79c7628898c71cda657165a136c
weezingq <rolegoci@yandex.ru>
2024-04-28 19:07:35 +0300

    dev06 done

---
+++ develop/dev06/cut.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fields := flag.String("f", "", "fields to select")
+	delimiter := flag.String("d", "\t", "delimiter")
+	separated := flag.Bool("s", false, "only lines with delimiter")
+	flag.Parse()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if *separated && !strings.Contains(line, *delimiter) {
+			continue
+		}
+
+		// Разделение строки на колонки
+		columns := strings.Split(line, *delimiter)
+
+		// Выбор запрошенных колонок
+		selectedColumns := selectColumns(columns, *fields)
+
+		// Вывод результата
+		fmt.Println(strings.Join(selectedColumns, *delimiter))
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
+
+// selectColumns выбирает запрошенные колонки из среза колонок.
+func selectColumns(columns []string, fields string) []string {
+	if fields == "" {
+		return columns
+	}
+
+	var selectedColumns []string
+	for _, field := range strings.Split(fields, ",") {
+		if strings.Contains(field, "-") {
+			rangeLimits := strings.Split(field, "-")
+			start, end := parseRangeLimit(rangeLimits[0]), parseRangeLimit(rangeLimits[1])
+			for i := start; i <= end; i++ {
+				selectedColumns = append(selectedColumns, columns[i-1])
+			}
+		} else {
+			index := parseRangeLimit(field)
+			selectedColumns = append(selectedColumns, columns[index-1])
+		}
+	}
+
+	return selectedColumns
+}
+
+// parseRangeLimit разбирает ограничение диапазона и обрабатывает ошибки.
+func parseRangeLimit(limit string) int {
+	index, err := strconv.Atoi(limit)
+	if err != nil || index < 1 {
+		fmt.Fprintln(os.Stderr, "invalid field:", limit)
+		os.Exit(1)
+	}
+	return index
+}
