2e22a13c31db4cd8a8474720c7b02d838b2f6fc0
weezingq <rolegoci@yandex.ru>
2024-05-02 17:32:41 +0300

    dev09 done

---
+++ develop/dev09/go.mod
@@ -0,0 +1,3 @@
+module my_wget
+
+go 1.22.0
---
+++ develop/dev09/task.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "wrong input: use\t wget \"link to website\"")
+		return
+	}
+	wget(os.Args[1])
+}
+
+func wget(link string) {
+	resp, err := http.Get(link)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	strs := strings.Split(link, "/")
+
+	var filename string
+	if strs[len(strs)-1] == "" {
+		filename = "index.html"
+	} else {
+		filename = strs[len(strs)-1]
+	}
+
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
